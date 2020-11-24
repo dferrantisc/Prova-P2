70f669d27c57d2d41be16b8c44f523a0f7d6f3b2
Daniela <Dferrantisc@hotmail.com>
2020-11-23 22:08:39 -0300

    Entidade, layout e activity

---
+++ app/src/main/java/com/example/prova_p2/Entidade/Livro.java
@@ -0,0 +1,24 @@
+package com.example.prova_p2.Entidade;
+
+public class Livro {
+
+    private String nome;
+    private Integer paginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+}
+
--- app/src/main/java/com/example/prova_p2/MainActivity.java
+++ app/src/main/java/com/example/prova_p2/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.prova_p2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nome;
+    EditText pagina;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = (EditText) findViewById(R.id.campoNome);
+        pagina = (EditText) findViewById(R.id.campoPagina);
+
     }
+
 }
